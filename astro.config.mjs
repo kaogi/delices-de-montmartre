16094a0ca5c6cfe431eb8f882bd6ff3aa63cd06f
kaogi <kaogit@gmail.com>
2025-11-14 20:40:17 +0100

    Remove Tailwind integration for build compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://délices-de-montmartre.pages.dev',
+  site: 'https://delices-de-montmartre.pages.dev',
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     sitemap(),
   ],
   output: 'static',
